231d3d809979b052d6806d84dbcd5eb148daeafa
atefth <atefth@gmail.com>
2014-11-28 02:06:18 +0600

    success

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,6 +38,9 @@ var grid = {
 		console.log("clearing : ["+x+", "+y+"]");
 		this._grid[x][y][0] = EMPTY;
 		this._grid[x][y][1] = 0;
+	},
+	direct: function (x, y, direction) {
+		this._grid[x][y][1] = direction;
 	}
 }
 
@@ -53,13 +56,16 @@ var head = {
 	get: function () {
 		return grid.get(this.x, this.y);
 	},
-	set: function (x, y, dir) {
-		this.x = x;
-		this.y = y;
-		grid.set(x, y, SNAKE, dir);
+	set: function (node, dir) {
+		this.x = node.x;
+		this.y = node.y;
+		grid.set(this.x, this.y, SNAKE, dir);
 	},
-	set: function (direction) {
-		this.direction = direction;
+	direction: function () {
+		return grid.get(this.x, this.y)[1];
+	},
+	direct: function (direction) {
+		grid.direct(this.x, this.y, direction);
 	}
 }
 
@@ -75,29 +81,36 @@ var tail = {
 	get: function () {
 		return grid.get(this.x, this.y);
 	},
-	set: function (x, y, dir) {
+	set: function (node) {
+		this.x = node.x;
+		this.y = node.y;
+	},
+	direction: function () {
+		return grid.get(this.x, this.y)[1];
+	},
+	clear: function () {
+		grid.clear(this.x, this.y);
+	},
+	shift: function (x, y) {
 		this.x = x;
 		this.y = y;
-		grid.set(x, y, SNAKE, dir);
 	}
 }
 
-//Reference to the Next position
+//Reference to the Next point
 var next = {
 	x: null,
 	y: null,
-	init: function (x, y, dir) {
+	init: function (x, y) {
 		this.x = x;
 		this.y = y;
-		grid.set(x, y, EMPTY, dir);
 	},
 	get: function () {
 		return grid.get(this.x, this.y);
 	},
-	set: function (x, y, dir) {
+	set: function (x, y) {
 		this.x = x;
 		this.y = y;
-		grid.set(x, y, EMPTY, dir);
 	}
 }
 
@@ -109,24 +122,65 @@ var snake = {
 		this.size = size;
 		this.direction = direction;
 	},
-	move: function () {		
-		if (this.size == 1) {
-			head.x = next.x;
-			head.y = next.y;
-			head.direction = next.direction;
-			this.updateNext();
-			grid.clear(tail.x, tail.y);
-			tail.set(head.x, head.y, head.direction);
-		}else{
+	move: function () {
+		if (next.get()[0] == FOOD) {
+			this.grow();
+		}else if(next.get()[0] == SNAKE){
 
+		}else if (this.size == 1) {
+			head.set(next, this.direction);
+			this.updateNext();
+			tail.clear();
+			tail.set(head);
+		}else{
+			console.log("head : [" + head.x + ", " + head.y + "]");
+			head.set(next, this.direction);
+			console.log("updated head : [" + head.x + ", " + head.y + "]");
+			this.updateNext();
+			var x = tail.x, y = tail.y, dir = tail.direction();
+			console.log("direction :" + dir);
+			switch(dir){
+				case UP:
+					y--;
+					break;
+				case DOWN:
+					y++;
+					break;
+				case LEFT:
+					x--;
+					break;
+				case RIGHT:
+					x++;
+					break;
+			}
+			if (x < 0) {
+				x = grid.cols - 1;
+			}else if(x >= grid.cols){
+				x = 0;
+			}
+			if (y < 0) {
+				y = grid.rows - 1;
+			}else if(y >= grid.rows){
+				y = 0;
+			}
+			tail.clear();
+			tail.shift(x, y);
 		}
 	},
 	change: function (direction) {
 		this.direction = direction;
+		head.direct(direction);
 		this.updateNext();
 	},
 	grow: function () {
-		
+		console.log("growing");
+		console.log("head : [" + head.x + ", " + head.y + "]");
+		head.set(next, this.direction);
+		console.log("updated head : [" + head.x + ", " + head.y + "]");
+		this.updateNext();
+		this.size++;
+		resetFood();
+		console.log("tail : [" + tail.x + ", " + tail.y + "]");
 	},
 	updateNext: function () {		
 		var x = head.x;		
@@ -155,10 +209,18 @@ var snake = {
 		}else if(y >= grid.rows){
 			y = 0;
 		}
-		next.x = x;
-		next.y = y;
-		next.direction = this.direction;
+		next.set(x, y);
 	}
+}
+
+function resetFood () {
+	var x = 0;
+	var y = 0;
+	while (grid.get(x, y)[0] != EMPTY) {
+		x = Math.floor(Math.random() * grid.cols);
+		y = Math.floor(Math.random() * grid.rows);
+	}
+	grid.set(x, y, FOOD, 0);
 }
 
 function init (cols, rows, value, direction) {
@@ -166,8 +228,9 @@ function init (cols, rows, value, direction) {
 	DIRECTION = direction;
 	head.init(2, 7, DIRECTION);
 	tail.init(2, 7, DIRECTION);
-	next.init(2, 6, DIRECTION);
+	next.init(2, 6);
 	snake.init(1, DIRECTION);
+	resetFood();
 	window.addEventListener("keydown", listener);
 	setInterval(function () {
 		loop();
@@ -209,12 +272,10 @@ function draw () {
 			if (grid.get(i, j)[0] == EMPTY) {
 				this.context.fillStyle = "#666";
 				this.context.fillRect((i * WIDTH)+(i * MARGIN),(j * HEIGHT)+(j * MARGIN), HEIGHT, WIDTH);
-			}
-			if (grid.get(i, j)[0] == SNAKE) {
+			}else if (grid.get(i, j)[0] == SNAKE) {
 				this.context.fillStyle = "#ABA";
 				this.context.fillRect((i * WIDTH)+(i * MARGIN),(j * HEIGHT)+(j * MARGIN), HEIGHT, WIDTH);
-			}
-			if (grid.get(i, j)[0] == FOOD) {
+			}else if (grid.get(i, j)[0] == FOOD) {
 				this.context.fillStyle = "#111";
 				this.context.fillRect((i * WIDTH)+(i * MARGIN),(j * HEIGHT)+(j * MARGIN), HEIGHT, WIDTH);
 			}
